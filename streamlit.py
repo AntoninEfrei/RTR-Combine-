a82b82e2af6f0f9840b3f088402afdc47fa08502
AntoninEfrei <antonin.guillocheau@efrei.net>
2024-04-18 19:27:05 +0200

    Update

--- streamlit.py
+++ streamlit.py
@@ -166,6 +166,163 @@ def get_spring24_geng_draft():
 
  ### DATA IMPORTS
 
+def get_games_player_stats(list_game_end_geng_compet, list_players):
+    # Initialize empty lists to store data
+    players = []
+    champions = []
+    kills = []
+    assists = []
+    deaths = []
+    kdas = []
+    kill_participations = []
+    damage_per_minutes = []
+    gold_per_minutes = []
+    team_damage_percentages = []
+    plates_takens = []
+    plates_losts = []
+    enemy_jgl_camp_killeds = []
+    vision_score_per_minutes = []
+    pink_ward_time_coverage_in_ennemy_or_rivers = []
+    control_wards_boughts = []
+    wards_placeds = []
+    wards_killeds = []
+    csm = []
+    wins = []
+    # Loop through each game in the list
+    for game in list_game_end_geng_compet:
+        for index in range(10):  # Assuming there are always 10 participants in a game
+            player_data = game['participants'][index]
+            player = player_data['riotIdGameName']
+
+            if player in list_players:
+                players.append(player)
+                champions.append(player_data['championName'])
+                kills.append(player_data['kills'])
+                assists.append(player_data['assists'])
+                deaths.append(player_data['deaths'])
+                kdas.append(player_data['challenges']['kda'])
+                kill_participations.append(player_data['challenges']['killParticipation'])
+                damage_per_minutes.append(player_data['challenges']['damagePerMinute'])
+                gold_per_minutes.append(player_data['challenges']['goldPerMinute'])
+                team_damage_percentages.append(player_data['challenges']['teamDamagePercentage'])
+                plates_takens.append(player_data['challenges']['turretPlatesTaken'])
+                plates_losts.append(player_data['challenges']['turretTakedowns'])
+                enemy_jgl_camp_killeds.append(player_data['challenges']['enemyJungleMonsterKills'])
+                vision_score_per_minutes.append(player_data['challenges']['visionScorePerMinute'])
+                try : 
+                    pink_ward_time_coverage_in_ennemy_or_rivers.append(player_data['challenges']['controlWardTimeCoverageInRiverOrEnemyHalf'])
+                except KeyError:
+                    pink_ward_time_coverage_in_ennemy_or_rivers.append(0)
+                control_wards_boughts.append(player_data['visionWardsBoughtInGame'])
+                wards_placeds.append(player_data['wardsPlaced'])
+                wards_killeds.append(player_data['wardsKilled'])
+                csm.append((player_data['totalMinionsKilled']+player_data['totalAllyJungleMinionsKilled']+player_data['totalEnemyJungleMinionsKilled'])/(game['gameDuration']/60))
+                wins.append(player_data['win'])
+
+    # Create a DataFrame
+    df = pd.DataFrame({
+        'Player': players,
+        'Champion': champions,
+        'KDA': kdas,
+        'Kills': kills,
+        'Deaths': deaths,
+        'Assists' : assists,
+        'Kill Participation': kill_participations,
+        'Damage Per Minute': damage_per_minutes,
+        'Gold Per Minute': gold_per_minutes,
+        'Team Damage Percentage': team_damage_percentages,
+        'Plates Taken': plates_takens,
+        'Plates Lost': plates_losts,
+        'Enemy Jungle Camp Killed': enemy_jgl_camp_killeds,
+        'Vision Score Per Minute': vision_score_per_minutes,
+        'Pink Ward Time Coverage': pink_ward_time_coverage_in_ennemy_or_rivers,
+        'Control Wards Bought': control_wards_boughts,
+        'Wards Placed': wards_placeds,
+        'Wards Killed': wards_killeds,
+        'Cs/min':csm,
+        'Win': wins
+    })
+
+    return df
+
+def player_champion_stats(df):
+    # Grouping data by champion
+    champion_group = df.groupby('Champion')
+
+    # Calculating statistics
+    champion_stats_df = pd.DataFrame()
+    champion_stats_df['Games'] = champion_group['Player'].count()
+    champion_stats_df['KDA'] = champion_group['KDA'].mean()
+    champion_stats_df['KP'] = champion_group['Kill Participation'].mean()*100
+    champion_stats_df['DmgShare'] = champion_group['Team Damage Percentage'].mean()*100
+    champion_stats_df['Gold/min'] = champion_group['Gold Per Minute'].mean()
+    champion_stats_df['Dmg/min'] = champion_group['Damage Per Minute'].mean()
+    champion_stats_df['Cs/min'] = champion_group['Cs/min'].mean()
+
+
+    gold_damage_ratios = []
+    for champion, group in champion_group:
+        gold_damage_ratio = (group['Damage Per Minute'] / group['Gold Per Minute']).mean()
+        gold_damage_ratios.append(gold_damage_ratio)
+
+    champion_stats_df['G/D ratio'] = gold_damage_ratios
+
+    champion_stats_df['W/R'] = champion_group['Win'].mean()*100
+    return champion_stats_df.sort_values(by='Games', ascending= False).round(1)
+
+def player_champion_vision_stats(df):
+    # Grouping data by champion
+    champion_group = df.groupby('Champion')
+
+    # Calculating vision statistics
+    vision_stats_df = pd.DataFrame()
+    vision_stats_df['Games'] = champion_group['Player'].count()
+    vision_stats_df['Wards Placed'] = champion_group['Wards Placed'].mean()
+    vision_stats_df['Wards Killed'] = champion_group['Wards Killed'].mean()
+    vision_stats_df['WardsLifetime'] = champion_group['Pink Ward Time Coverage'].mean()
+    vision_stats_df['ControlWards Bought'] = champion_group['Control Wards Bought'].mean()
+    vision_stats_df['VisionScore/Minute'] = champion_group['Vision Score Per Minute'].mean()
+
+    return vision_stats_df.sort_values(by='Games', ascending= False).round(1)
+
+def player_stats(df):
+
+    # Player stats
+    player_stats_dict = {
+        'Average KDA': [df['KDA'].mean()],
+        'Average Kill Participation': [df['Kill Participation'].mean() * 100],
+        'Average Team Damage Share': [df['Team Damage Percentage'].mean() * 100],
+        'Average Gold/Minute': [df['Gold Per Minute'].mean()],
+        'Average Damage/Minute': [df['Damage Per Minute'].mean()],
+        'Average Cs/Minute': [df['Cs/min'].mean()]
+
+    }
+
+    # Calculate gold/damage ratio for all games
+    if not df.empty:
+        valid_rows = df[(df['Gold Per Minute'] != 0) & (df['Damage Per Minute'] != 0)]
+        if not valid_rows.empty:
+            gold_damage_ratio = (valid_rows['Damage Per Minute'] / valid_rows['Gold Per Minute']).mean()
+            player_stats_dict['Average Gold/Damage Ratio'] = [gold_damage_ratio]
+
+    player_stats_df = pd.DataFrame(player_stats_dict)
+    player_stats_df = player_stats_df.set_index('Average KDA')
+
+    # Vision stats
+    vision_stats_dict = {
+        'Avg Wards Placed': [df['Wards Placed'].mean()],
+        'Avg Wards Killed': [df['Wards Killed'].mean()],
+        'Avg Control Ward Lifetime': [df['Pink Ward Time Coverage'].mean()],
+        'Avg Control Wards Bought': [df['Control Wards Bought'].mean()],
+        'Avg Vision Score/min': [df['Vision Score Per Minute'].mean()]
+    }
+    
+    vision_stats_df = pd.DataFrame(vision_stats_dict)
+    vision_stats_df = vision_stats_df.set_index('Avg Wards Placed')
+    return player_stats_df, vision_stats_df
+
+    
+
 # DATA IMPORTS
 
 with open("list_game_end_geng_compet.json",'r') as file:
@@ -173,29 +330,49 @@ with open("list_game_end_geng_compet.json",'r') as file:
 with open("list_game_timeline_geng_compet.json",'r') as file:
     list_game_timeline_geng_compet = json.load(file)
 
-df_spring24_geng_draft = get_spring24_geng_draft()
+list_players = ['GENKiin','GENCanyon','GENChovy','GENPeyz','GENLehends']
 
+Kiin_puuid = "Vpq3Y6Nns_bME-adpgmXfI89CIH3k0MCfDbrWZN2ASTuE8FlBit7rwbz3kzy_t4T8kgpGAo51asizA"
+Canyon_puuid = "C7SQvoTcKkF1B2dZxm9Oo5yiRFtp7M5t0QNur_5jiPm1EymkWiVlTQAISnUeqmvqOQcm1QvKjTIYkg"
+Chovy_puuid = "0ZeAFCtsYyae-HzXEvKujLs8XQUG-1fUjJiybfBAW1cPHR9ZPMU0hiQlafxPcyNDYkiYW4J-pBkMHQ"
+Peyz_puuid  = "KSM9lFqeaD6TAzg5c2hkxxbMhJ01-6d_w6dNNiCaY-vsPRcjRbgXjq1EjhiaPpvW4Da07vFeduKkRQ"
+Lehends_puuid = "sbe811AZXiBHw3UDQFn8TMHINYaKCNrMJQM0xUb-K7wBgHVvZ2p0EXVAOtWE-agBX3CNxyMwtjwuwA"
+
+list_puuid = [Kiin_puuid,Canyon_puuid,Chovy_puuid,Peyz_puuid,Lehends_puuid]
 
 # Stylish header
-st.markdown(
+st.set_page_config(layout="wide")
+
+
+with st.sidebar.expander("Navigation"):
+    # Display checkboxes for options within the expander
+
+    page = st.radio('Navigation',["Home Page","Reporting Draft","Reporting In Game","SoloQ Overview","Player Focus"])
+
+if page == 'Home Page':
+    st.markdown(
     """
-    <h2 style='text-align: center; color: #f63366;'>Gen.G Reporting made by Gulldiz</h2>
+    <h1 style='text-align: center; color: #f63366;'>Gen.G Reporting</h1>
     <h4 style='text-align: center; color: #ffffff;'>LIGHT THEME RECOMMENDED</h4>
+    <h3 style='text-align: center;'>Running the Rift project made By Gulldiz</h3>
     <hr style='margin-bottom: 20px;'>
     """,
     unsafe_allow_html=True
 )
+    st.markdown('')
+    st.markdown('')
+    st.write("French young analyst started 2 months ago discovering RIOT's data world. I'm named Antonin aka Gulldiz I am 23 yo currently at my last year of a Master's degree in DataScience in Paris. I am pleased to present you my work of the last 4 days. I started from the principle that I present my work to the coach of a NACL team that will play against Gen.G in a Fearless Final (Which i'm new to - fun mode tho). I've taken some decisions about what to show and what to not because the timeline is kinda short - but enough.")
+    st.markdown('')
+    st.markdown('')
 
+    
+    st.write('Shoutout to RTR Combine project that offers us opportunities to work and showcase it. It has been a pleasure to work in this case study.')
+    st.write('Any feedbacks or recommendations is warmly welcome - do not hesitate on discord (Gulldiz) or elsewhere')
 
-
-page = st.sidebar.radio("Navigation", ("Home Page","Reporting Draft","Reporting In Game","SoloQ Overview"))
-
-
-if page == 'Home Page':
-
-    st.write("Homepage for Running the rift project made By Gulldiz"
-)
 elif page == "Reporting Draft":
+
+    df_spring24_geng_draft = get_spring24_geng_draft()
+    st.write(df_spring24_geng_draft)
 
     st.title('Reporting Gen.G Draft')
     
@@ -212,13 +389,52 @@ elif page == "Reporting Draft":
     st.write(matrix_percentages_blue)
     st.write(matrix_percentages_red)
 
-
 elif page == "Reporting In Game":
     
     st.title('Reporting In Game')
   
-    
-    
 elif page == "SoloQ Overview":
    
    st.title('Reporting SoloQ')
+
+elif page == "Player Focus":
+
+
+    
+
+    col_tab1,col_tab2 = st.columns([3,8])
+
+
+   
+    with col_tab1:
+
+        with st.expander("Choose Player", expanded = False):
+            # Display options within the expander
+            player = st.radio("Players", list_players)
+
+
+    df_players_stats = get_games_player_stats(list_game_end_geng_compet,list_players) #from list of dict to all GenG players stats only
+    df_players_stats_scope = df_players_stats[df_players_stats['Player'] == player] #focus on choosen player
+    
+    with col_tab2:
+        # PLAYER WISE FOCUS
+   
+        df_player, df_player_vision = player_stats(df_players_stats_scope)
+        st.write('Player stats')
+        st.write(df_player)
+        
+        st.write('Vision stats')
+        st.write(df_player_vision)
+
+
+    # CHAMPIONS WISE FOCUS
+    st.title('Champion Focus')
+    st.write('Champion Stats')
+    df_player_champion_stats = player_champion_stats(df_players_stats_scope)
+    st.write(df_player_champion_stats)
+    if player in ['GENLehends','GENCanyon']:
+        st.write('Vision Stats')
+        df_player_vision_stats = player_champion_vision_stats(df_players_stats_scope)
+        st.write(df_player_vision_stats)
+
+    # HEAT MAPS 
